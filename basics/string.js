27a6ed305ce1080dd536746da049fa0f80704aed
Jayp664 <163424065+Jayp664@users.noreply.github.com>
2025-05-15 14:25:43 +0000

    strings

---
+++ basics/string.js
@@ -0,0 +1,46 @@
+const name = "Jay"
+const surname = "Panchal"
+
+// console.log(name + surname);
+
+console.log(`My name is ${surname} ${name} Ashishbhai`)
+
+const gameName = new String("Jay panchal")
+
+console.log(gameName);
+
+// console.log(gameName[2]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.substring(2,6));
+// console.log(gameName.trim());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('c'));
+
+//const another = gameName.slice(-2,2) //plus point is we can assign negative value as well
+// console.log(gameName.split(' '));
+// console.log(gameName.replace(' ','-'));
+// console.log(gameName.includes('panchal'));
+// console.log(gameName.includes('suresh'));
+
+console.log(gameName.includes('panchal'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
