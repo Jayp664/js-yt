517c9bc1c8826cec3353aa1e3c9567919dfdcf7e
Jayp664 <163424065+Jayp664@users.noreply.github.com>
2025-05-15 08:41:22 +0000

    conversion and datatype

---
+++ basics/conversion.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueinNuber = Number(score)
+
+console.log(typeof valueinNuber);
+console.log(valueinNuber);
+
+//"33" => 33
+//"33abc" => NaN
+//"null" => 0
+//true => 1 ; false => 0
+
+let isLoggedin = ""
+
+let booleanInLoggedin = Boolean(isLoggedin)
+
+console.log(booleanInLoggedin);
+
+//1 => true ; 0 => false
+//"" => false
+//"Jay" => true
+
---
+++ basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict";//treate all js code as newer version
+
+let name = "Jay"
+let age = 21
+let isLoggedin = true
+let state = null 
+
+//number 2 to power of 53 
+//bigint
+//string => "Jay"
+//boolean => true,false
+//null => standalon value
+//undefined
+//symbol => unique 
+
+
+//object
+
+console.log(typeof null);
+
+//---------------Important--------------//
+//type of null is object 
