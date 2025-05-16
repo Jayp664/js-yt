a639dbc4bda35de728e91ef045f3aa7b4880e7b3
Jayp664 <163424065+Jayp664@users.noreply.github.com>
2025-05-16 10:35:48 +0000

    num,math,date,time

---
+++ basics/Date_and_time.js
@@ -0,0 +1,29 @@
+let myDate =  new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// const mycreated = new Date(2004,2 - 1,22);
+// const mycreated = new Date(2004,2 - 1,22,5,30);
+const mycreated = new Date("2004-02-22");
+// console.log(mycreated.toLocaleString());
+
+const myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(mycreated.getTime());
+//console.log(Math.floor(myTimestamp/1000)); //convert in seconds IMP
+
+
+// console.log(myDate.getMonth() + 1);
+// console.log(myDate.getDay());
+
+
+
+
+
+
+
+ 
